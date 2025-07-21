4220ce2a26f70b82cc60d7b8cfcbbe7000e4a1ab
cordy001 <mglgain@gmail.com>
2025-07-22 00:14:31 +0800

    Walking

--- src/components/scene.js
+++ src/components/scene.js
@@ -15,10 +15,18 @@ export default function createScene(Phaser) {
         frameWidth: 32,
         frameHeight: 32 
       })
-      this.load.spritesheet("player_walk_front", "/walk_front.png", {
+      this.load.spritesheet("player_idle2", "/idle2.png", {
         frameWidth: 32,
         frameHeight: 32 
       })
+      this.load.spritesheet("player_walk_front", "/walk_front.png", {
+        frameWidth: 32,
+        frameHeight: 32 
+      });
+      this.load.spritesheet("player_walk_back", "/walk_back.png", {
+        frameWidth: 32,
+        frameHeight: 32 
+      });
     }
     
     create() {
@@ -38,25 +46,80 @@ export default function createScene(Phaser) {
       });
 
       this.anims.create({
+        key: "walk_back",
+        frames: this.anims.generateFrameNumbers("player_walk_back", { start: 0, end: 5 }),
+        frameRate: 10,
+        repeat: -1 // Loop forever
+      });
+
+
+      this.anims.create({
         key: "idle",
         frames: this.anims.generateFrameNumbers("player_idle", { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1 // Loop forever
       });
 
-      // Add the sprite and play the animation
-      const player = this.add.sprite(400, 100, "player_walk");
-      player.anims.play("walk");
-      player.setScale(7); // Makes the sprite 2x bigger
+       this.anims.create({
+        key: "idle2",
+        frames: this.anims.generateFrameNumbers("player_idle2", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1 // Loop forever
+      });
 
-      const idlePlayer = this.add.sprite(400, 300, "player_idle");
-      idlePlayer.anims.play("idle");
-      idlePlayer.setScale(7); // Makes the sprite 2x bigger
+      // Add player sprite
+      this.player = this.add.sprite(400, 300, "player_idle").setOrigin(0.5, 0.5);
+      this.player.anims.play("idle");
+      this.player.setScale(5);
 
-      const frontPlayer = this.add.sprite(400, 500, "player_walk_front");
-      frontPlayer.anims.play("walk_front");
-      frontPlayer.setScale(7); // Makes the sprite 2x bigger
+      // Add keyboard input
+      this.cursors = this.input.keyboard.createCursorKeys();
+      this.speed = 5;
     }
-    update() {}
+    update() {
+      if (!this.player || !this.cursors) return;
+
+      let moving = false;
+
+      // Track last direction
+      if (!this.lastDirection) this.lastDirection = "down";
+
+      if (this.cursors.left.isDown) {
+        this.player.x -= this.speed;
+        this.player.anims.play("walk", true);
+        this.player.flipX = true;
+        this.lastDirection = "left";
+        moving = true;
+      } else if (this.cursors.right.isDown) {
+        this.player.x += this.speed;
+        this.player.anims.play("walk", true);
+        this.player.flipX = false;
+        this.lastDirection = "right";
+        moving = true;
+      } else if (this.cursors.up.isDown) {
+        this.player.y -= this.speed;
+        this.player.anims.play("walk_back", true);
+        this.lastDirection = "up";
+        this.player.flipX = true
+        moving = true;
+      } else if (this.cursors.down.isDown) {
+        this.player.y += this.speed;
+        this.player.anims.play("walk_front", true);
+        this.lastDirection = "down";
+        this.player.flipX = false
+        moving = true;
+      }
+
+      // Play idle animation based on last direction
+      if (!moving) {
+        if (this.lastDirection === "left" || this.lastDirection === "right") {
+          this.player.anims.play("idle2", true);
+          this.player.flipX = this.lastDirection === "left";
+        } else {
+          this.player.anims.play("idle", true);
+          this.player.flipX = false;
+        }
+      }
+    } 
   };
 }
