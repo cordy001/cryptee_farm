bcdefa4f43cfb58e002637f71bdfeb830edc5b61
cordy <mglgain@gmail.com>
2025-08-05 03:25:08 +0800

    OOP ricing

--- src/module/scene1/players/player_create.js
+++ src/module/scene1/players/player_create.js
@@ -1,56 +1,33 @@
 export default function PlayerCreate(scene) {
-    
-    // Create the animation using all 6 frames
-      scene.anims.create({
-        key: "walk",
-        frames: scene.anims.generateFrameNumbers("player_walk", { start: 0, end: 5 }),
-        frameRate: 10,
-        repeat: -1 // Loop forever
-      });
 
-      scene.anims.create({
-        key: "walk_front",
-        frames: scene.anims.generateFrameNumbers("player_walk_front", { start: 0, end: 5 }),
-        frameRate: 10,
-        repeat: -1 // Loop forever
-      });
+    const anims = [
+      {key: "walk", prefix: "player_walk", end: 5},
+      {key: "walk_front", prefix: "player_walk_front", end: 5},
+      {key: "walk_back", prefix: "player_walk_back", end: 5},
+      {key: "idle", prefix: "player_idle", end: 3},
+      {key: "idle2", prefix: "player_idle2", end: 3},
+      {key: "idle3", prefix: "player_idle3", end: 3}
+    ];
 
-      scene.anims.create({
-        key: "walk_back",
-        frames: scene.anims.generateFrameNumbers("player_walk_back", { start: 0, end: 5 }),
-        frameRate: 10,
-        repeat: -1 // Loop forever
-      });
+    for (const {key, prefix, end } of anims) {
+        // 1) Load the sprite sheet for the player animations
+        if (!scene.anims.exists(key)) {
+          scene.anims.create({
+            key: key,
+            frames: scene.anims.generateFrameNumbers(prefix, { start: 0, end: end }),
+            frameRate: 10,
+            repeat: -1 // Loop forever
+          })
+        }
+    }
+    // 2) Create the player as a physics sprite
+    scene.player = scene.physics.add.sprite(144, 160, 'player_idle')
+      .setOrigin(0, 0) // top-left origin simplifies everything
+      .setDisplaySize(16, 16) // scale to 16x16 pixels
+      .setCollideWorldBounds(true);
 
-      scene.anims.create({
-        key: "idle",
-        frames: scene.anims.generateFrameNumbers("player_idle", { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-      });
-
-       scene.anims.create({
-        key: "idle2",
-        frames: scene.anims.generateFrameNumbers("player_idle2", { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1 
-      });
-
-      scene.anims.create({
-        key: "idle3",
-        frames: scene.anims.generateFrameNumbers("player_idle3", { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-      })
-
-      // 2) Create the player as a physics sprite
-      scene.player = scene.physics.add.sprite(144, 160, 'player_idle')
-        .setOrigin(0, 0) // top-left origin simplifies everything
-        .setDisplaySize(16, 16) // scale to 16x16 pixels
-        .setCollideWorldBounds(true);
-
-      // Collider setup — must be done on the body
-      scene.player.body.setSize(4, 2, true);    // collider: full width, short height
-      scene.player.setDepth(scene.player.y);
-      scene.player.setCollideWorldBounds(true);
+    // Collider setup — must be done on the body
+    scene.player.body.setSize(4, 2, true);    // collider: full width, short height
+    scene.player.setDepth(scene.player.y);
+    scene.player.setCollideWorldBounds(true);
 }
